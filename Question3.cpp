6fd3967016cd5ebcecefb1d2cc6aa6a1811c23a2
darkdenim <darkdenim@gmail.com>
2016-02-20 13:23:53 +0200

    Added files via upload

---
+++ Question3.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+int HighestMark(int a1, int a2, int a3)
+{
+    int highest = 0;
+    if (a1 > a2 && a1 > a3) highest = a1;
+    if (a2 > highest && a2 > a3) highest = a2;
+    if (a3 > highest) highest = a3;
+    return highest;
+}
+
+int main()
+{
+    ofstream outFile;
+    outFile.open("yearmark.dat");
+    
+    // open marks.dat in read mode
+    ifstream file;
+    file.open("marks.dat");
+    cout << "Reading marks.dat ..." << endl; 
+    
+    int items[4];
+    size_t pos = 0;
+    string delimiter = " ";
+    string token;
+    string s;
+    while (getline(file, s))
+    {
+        int i = 0;
+        cout << s << endl;
+        while ((pos = s.find(delimiter)) != string::npos) {
+            token = s.substr(0, pos);
+            s.erase(0, pos + delimiter.length());
+            items[i] = atoi(token.c_str());
+            i++;
+        }
+        items[i] = atoi(s.c_str());
+        for (int i = 0; i < sizeof(items)/sizeof(*items); i++)
+        {
+            cout << items[i] << endl;
+        }
+        
+        int sn = items[0];
+        int a1 = items[1];
+        int a2 = items[2];
+        int a3 = items[3];
+        
+        double avg = (a1 + a2 + a3) / 3;
+        double ym = avg * 0.4 + (HighestMark(a1, a2, a3) * 0.6);
+        outFile << sn << " " << avg << " " << ym << endl;
+        
+    }
+}
